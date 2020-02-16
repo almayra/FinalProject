9ca7224f35ecb23b27548e279605f56aae45ae4b
aya <56827395+almayra@users.noreply.github.com>
2020-02-16 10:38:41 +0700

    uuu

--- src/components/jumbotron3.jsx
+++ src/components/jumbotron3.jsx
@@ -12,6 +12,7 @@ export class Jumbotron3 extends Component {
         return (
             <div className='App-jumbotron2'>
                <div className='App-jumbotron2'>
+
                 <div data-aos="fade-up" data-aos-duration="1000">
                     <span className='dot' style={{marginTop:'2cm',marginLeft:'6cm'}}/>
                     <img src={ICON5} 
@@ -20,30 +21,34 @@ export class Jumbotron3 extends Component {
                     />
                     <h4 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='f'>f. Art Director</h4>
                 </div>
+
                 <div data-aos="fade-up" data-aos-duration="1500">
                     <span className='dot' style={{marginTop:'2cm',marginLeft:'1.5cm'}}/>
                     <img src={ICON6} 
                     style={{width:'130px',height:'120px'}} 
                     className='icon2'
                     />
-                    <h2 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='g1'>g. Content Provider</h2>
+                    <h2 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='h1'>g. Content Provider</h2>
                 </div>
+
                 <div data-aos="fade-up" data-aos-duration="2000">
-                    <span className='dot' style={{marginTop:'2cm',marginLeft:'0.5cm'}}/>
+                    <span className='dot' style={{marginTop:'2cm',marginLeft:'0cm'}}/>
                     <img src={ICON7} 
                     style={{width:'130px',height:'120px'}} 
                     className='icon1'
                     />
-                    <h2 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='h1'>i. Creative Group Head</h2>
+                    <h2 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='g1'>i. Creative Group Head</h2>
                 </div>
+
                 <div data-aos="fade-up" data-aos-duration="2500">
-                    <span className='dot' style={{marginTop:'2cm',marginLeft:'1cm'}}/>
+                    <span className='dot' style={{marginTop:'2cm',marginLeft:'0cm'}}/>
                     <img src={ICON8} 
                     style={{width:'120px',height:'135px'}} 
                     className='icon3'
                     />
                     <h2 style={{color:'white',fontSize:'18px',fontWeight:'900px'}} className='h1'>i. Perorangan</h2>
                 </div>
+
                 <div style={{marginTop:'8cm',marginLeft:'-26cm'}}>
                     <img 
                     src={LINE}
